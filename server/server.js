5021b34fdefe331702a72f0fe8c15701c6545125
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2018-09-26 20:44:15 +0530

    Added emit new message on create message

--- server/server.js
+++ server/server.js
@@ -15,12 +15,11 @@ io.on('connection', (socket) => {
 
   socket.on('createMessage', (msg) => {
     console.log('createMessage', msg);
-  });
-
-  socket.emit('newMessage', {
-    from: 'akshay',
-    text: 'hi from server',
-    createdAt: new Date().getTime()
+    io.emit('newMessage', {
+      from: msg.from,
+      text: msg.text,
+      createdAt: new Date().getTime()
+    });
   });
 
   socket.on('disconnect', () => {
