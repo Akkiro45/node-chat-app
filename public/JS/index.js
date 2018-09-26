e1ae7493a72584f23425b5f5140d7b5c1fd9fdf9
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2018-09-26 20:54:11 +0530

    removed header from index.html

---
+++ public/JS/index.js
@@ -0,0 +1,13 @@
+const socket = io();
+
+socket.on('connect', function() {
+  console.log('Connected to server.');
+});
+
+socket.on('newMessage', function(msg) {
+  console.log('newMessage ', msg);
+});
+
+socket.on('disconnect', function() {
+  console.log('Disconnected  from server.');
+});
