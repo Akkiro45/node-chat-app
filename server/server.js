5001c625abc52dfa86781995bb14466a8c73e459
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2018-09-26 19:47:52 +0530

    Added socket io and connect, disconnect event

--- server/server.js
+++ server/server.js
@@ -1,16 +1,28 @@
 const path = require('path');
+const http = require('http');
 const express = require('express');
+const socketIO = require('socket.io');
 
+// Link: https://cryptic-anchorage-76734.herokuapp.com/
 const publicPath = path.join(__dirname, '../public');
 const app = express();
 const port = process.env.PORT || 3000;
+const server = http.createServer(app);
+const io = socketIO(server);
+
+io.on('connection', (socket) => {
+  console.log('New user connected.');
+  socket.on('disconnect', () => {
+    console.log('Disconnected clients');
+  });
+});
 
 // Middleware
 app.use(express.static(publicPath));
 
 // Routes
 
-app.listen(port, (e) => {
+server.listen(port, (e) => {
   if(e) {
     console.log('Unable to connect to server.');
   } else {
