586736db44c3a57b839710ec3ede2f6b11d1b148
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2018-09-29 19:54:17 +0530

    Added generateMessage utility

--- server/server.js
+++ server/server.js
@@ -3,6 +3,8 @@ const http = require('http');
 const express = require('express');
 const socketIO = require('socket.io');
 
+const {generateMessage} = require('./utils/message');
+
 // Link: https://cryptic-anchorage-76734.herokuapp.com/
 const publicPath = path.join(__dirname, '../public');
 const app = express();
@@ -10,39 +12,28 @@ const port = process.env.PORT || 3000;
 const server = http.createServer(app);
 const io = socketIO(server);
 
+// Middleware
+app.use(express.static(publicPath));
+
+// SocketIO
+
 io.on('connection', (socket) => {
   console.log('New user connected.');
 
-  socket.emit('newMessage', {
-    text: 'Welcome to chat app.'
-  });
+  socket.emit('newMessage', generateMessage('Admin', 'Welcome to chat app.'));
 
-  socket.broadcast.emit('newMessage', {
-    from: 'Admin',
-    text: 'New User Joined'
-  });
+  socket.broadcast.emit('newMessage', generateMessage('Admin', 'New User Joined.'));
 
   socket.on('createMessage', (msg) => {
     console.log('createMessage', msg);
-    io.emit('newMessage', {
-      from: msg.from,
-      text: msg.text,
-      createdAt: new Date().getTime()
-    });
-    // socket.broadcast.emit('newMessage', {
-    //   from: msg.from,
-    //   text: msg.text,
-    //   createdAt: new Date().getTime()
-    // });
+    io.emit('newMessage', generateMessage(msg.from, msg.text));
+    // socket.broadcast.emit('newMessage', generateMessage(msg.from, msg.text));
   });
 
   socket.on('disconnect', () => {
     console.log('Disconnected clients');
   });
 });
-
-// Middleware
-app.use(express.static(publicPath));
 
 // Routes
 
