275e7717bfaa2728fa56984a9477caa6ba46d0f7
Akkiro45 <32303778+Akkiro45@users.noreply.github.com>
2018-09-30 15:20:30 +0530

    Added auto scroll

--- public/JS/index.js
+++ public/JS/index.js
@@ -1,5 +1,19 @@
 const socket = io();
 
+function scrollToBottom() {
+  const messages = $('#messages');
+  const newMessage = messages.children('li:last-child');
+  const clientHeight = messages.prop('clientHeight');
+  const scrollTop = messages.prop('scrollTop');
+  const scrollHeight = messages.prop('scrollHeight');
+  const newMessageHeight = newMessage.innerHeight();
+  const lastMessageHeight = newMessage.prev().innerHeight();
+
+  if((clientHeight + scrollTop + newMessageHeight + lastMessageHeight) >= scrollHeight) {
+    messages.scrollTop(scrollHeight);
+  }
+}
+
 socket.on('connect', function() {
   console.log('Connected to server.');
 });
@@ -13,6 +27,7 @@ socket.on('newMessage', function(msg) {
     createdAt: formattedTime
   });
   $('#messages').append(html);
+  scrollToBottom();
   // const formattedTime = moment(msg.createdAt).format('h:mm a');
   // console.log('newMessage ', msg);
   // const li = $('<li></li>');
@@ -29,6 +44,7 @@ socket.on('newLocationMessage', function(msg) {
     url: msg.url
   });
   $('#messages').append(html);
+  scrollToBottom();
   // const formattedTime = moment(msg.createdAt).format('h:mm a');
   // const li = $('<li></li>');
   // const a = $('<a target="_blank">My cuurent location.</a>')
